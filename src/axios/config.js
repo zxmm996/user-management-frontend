48ca193056dc48a89b224e430037454c88cc95f8
Zxmm <644521733@qq.com>
2018-09-30 16:38:58 +0800

    Update config.js
    modify axios config

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,17 +1,10 @@
 export default {
-  method: 'post',
   // 基础url前缀
   baseURL: 'http://192.168.19.84:3000',
   // 请求头信息
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
   },
-  // 参数
-  data: {},
   // 设置超时时间
   timeout: 10000,
-  // 携带凭证
-  withCredentials: false,
-  // 返回数据类型
-  responseType: 'json'
 }
